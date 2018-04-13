d7d3ad21797232c02fb2a2eb4b313cf3b2bca848
Taigrom Andrade <taigrom.andrade@aec.com.br>
2018-04-13 17:26:55 -0300

    habilitando openiddict

---
+++ Models/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Interfaces
+{
+    public interface IAuditableEntity
+    {
+        string CreatedBy { get; set; }
+        string UpdatedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime UpdatedDate { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AspNet.Security.OpenIdConnect.Primitives;
+using AspNet.Security.OAuth.Validation;
+using Microsoft.AspNetCore.Identity;
+using Models;
 
 namespace AngularApp
 {
@@ -22,6 +26,60 @@ namespace AngularApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                //options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"], b => b.MigrationsAssembly("QuickApp"));
+                options.UseOpenIddict();
+            });
+
+            // add identity
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // // Configure Identity options and password complexity here
+            // services.Configure<IdentityOptions>(options =>
+            // {
+            //     // User settings
+            //     options.User.RequireUniqueEmail = true;
+
+            //     //    //// Password settings
+            //     //    //options.Password.RequireDigit = true;
+            //     //    //options.Password.RequiredLength = 8;
+            //     //    //options.Password.RequireNonAlphanumeric = false;
+            //     //    //options.Password.RequireUppercase = true;
+            //     //    //options.Password.RequireLowercase = false;
+
+            //     //    //// Lockout settings
+            //     //    //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+            //     //    //options.Lockout.MaxFailedAccessAttempts = 10;
+
+            //     options.ClaimsIdentity.UserNameClaimType = OpenIdConnectConstants.Claims.Name;
+            //     options.ClaimsIdentity.UserIdClaimType = OpenIdConnectConstants.Claims.Subject;
+            //     options.ClaimsIdentity.RoleClaimType = OpenIdConnectConstants.Claims.Role;
+            // });
+
+            // Register the OpenIddict services.
+            services.AddOpenIddict(options =>
+            {
+                options.AddEntityFrameworkCoreStores<ApplicationDbContext>();
+                options.AddMvcBinders();
+                options.EnableTokenEndpoint("/connect/token");
+                options.AllowPasswordFlow();
+                options.AllowRefreshTokenFlow();
+                options.DisableHttpsRequirement();
+                //options.AddSigningKey(new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes(Configuration["STSKey"])));
+            });
+
+
+            // services.AddAuthentication(options =>
+            // {
+            //     options.DefaultAuthenticateScheme = OAuthValidationDefaults.AuthenticationScheme;
+            //     options.DefaultChallengeScheme = OAuthValidationDefaults.AuthenticationScheme;
+            // }).AddOAuthValidation();
+
+
             // Add cors
             services.AddCors();
 
