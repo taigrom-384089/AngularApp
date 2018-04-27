2ca134fff88be34a4114d1150f1b3100f6c058bc
Taigrom Andrade <taigrom.andrade@aec.com.br>
2018-04-27 17:30:07 -0300

    inclusao de authorize em sampledata

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AngularApp.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
